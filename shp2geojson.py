b864ae27abbaab527f5f30bd60ec7d939509c58c
Eric van Zanten <eric@bahai.us>
2013-08-25 08:07:36 -0500

    Added an example (mainly to see if github chokes) and added the ability to import blocks and tracts

--- shp2geojson.py
+++ shp2geojson.py
@@ -6,16 +6,22 @@ import zipfile
 from cStringIO import StringIO
 from itertools import izip_longest
 
+ENDPOINT = 'http://www2.census.gov/geo/tiger/TIGER2010'
+
+SHAPE_LOOKUP = {
+    'blocks': 'TABBLOCK',
+    'tracts': 'TRACT'
+}
+
 def grouper(iterable, n, fillvalue=None):
     args = [iter(iterable)] * n
     return izip_longest(*args, fillvalue=fillvalue)
 
-def dump_shapes(fips):
-    u = 'http://www2.census.gov/geo/tiger/TIGER2010/TABBLOCK/2010'
-    url = '%s/tl_2010_%s_tabblock10.zip' % (u, fips)
+def dump_shapes(fips, shape_type, outdir):
+    url = '%s/%s/2010/tl_2010_%s_%s10.zip' % (ENDPOINT, shape_type.upper(), fips, shape_type.lower())
     req = requests.get(url)
     if req.status_code != 200:
-        print 'Unable to fetch census block shape data for %s' % fips
+        print 'Unable to fetch census block shape data for %s from %s' % (fips, url)
     else:
         zf = StringIO(req.content)
         shp = StringIO()
@@ -39,7 +45,7 @@ def dump_shapes(fips):
                     geoid = record.record[4]
                     dump = {'type': 'Feature', 'geometry': record.shape.__geo_interface__}
                     geo['features'].append(dump)
-        f = open('out/%s.geojson' % fips, 'wb')
+        f = open('%s/%s_%s.geojson' % (outdir, fips, shape_type), 'wb')
         f.write(json.dumps(geo))
 
 if __name__ == "__main__":
@@ -55,11 +61,15 @@ if __name__ == "__main__":
         help="""
             Relative path to directory where you want to output GeoJSON.
             Created if it doens't already exist.""")
+    parser.add_argument('--shape_type', choices=['blocks', 'tracts'], required=True,
+        help="""
+            Type of shapes you want to dump.""")
     args = parser.parse_args()
     counties = args.counties.split(',')
     try:
         os.mkdir(args.outdir)
     except OSError:
         pass
+    shape_type = SHAPE_LOOKUP[args.shape_type]
     for fips in counties:
-        dump_shapes(fips)
+        dump_shapes(fips, shape_type, args.outdir)
